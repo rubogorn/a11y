959c1ff8873e4e44d0c2ab4b1e54d518d8cffb9f
rubocop <ruben@granadoshughes.com>
2024-11-05 13:42:59 +0100

    deleted redundant files

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,5 @@
 # src/__init__.py
 from .crew import WCAGTestingCrew
-from .tools.wcag_tools import WCAGTestingTools 
 from .tools.result_processor import TestResultProcessor
 
 
@@ -8,6 +7,5 @@ __version__ = "0.1.0"
 
 __all__ = [
     'WCAGTestingCrew',
-    'WCAGTestingTools',
     'TestResultProcessor'
 ]
